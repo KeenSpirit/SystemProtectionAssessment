ff2ee9fd44f3f61684a960193f85ea714ed250c6
dp072 <danjp62@hotmail.com>
2026-08-13 23:18:58 +1000

    dev update 7

---
+++ prot_coordination/prot_coord.py
@@ -0,0 +1,12 @@
+"""
+
+For the range of fault currents seen by the primary device,
+Calculate the primary device trip time
+Calculate the back-up device trip time
+Calculate the grading margin
+Store results
+"""
+
+
+def prot_coordination():
+    pass
